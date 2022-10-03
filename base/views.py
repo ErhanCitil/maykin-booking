892a985cc61156e6c0e624683810b8811e2d45fd
Erhan <erhancitil94@gmail.com>
2022-10-03 12:31:11 +0200

    Data added to view

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Data
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'data': Data.objects.all()
+    }
+    return render(request, 'index.html', context)
