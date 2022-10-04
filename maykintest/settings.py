76a9dd94ce5e4af764e6595c5142b0c08d1d4079
Erhan <erhancitil94@gmail.com>
2022-10-04 19:12:04 +0200

    Added favicon

--- maykintest/settings.py
+++ maykintest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +30,9 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new
 
 INSTALLED_APPS = [
     'django.contrib.admin',
