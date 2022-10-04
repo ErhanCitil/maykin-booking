e2dde13cdb1a05eecff20f00396c88ea6e96f96d
Erhan <erhancitil94@gmail.com>
2022-10-04 08:51:34 +0200

    Added 404 page

--- maykintest/settings.py
+++ maykintest/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'base',
 ]
 
+STATIC_URL = 'static/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
