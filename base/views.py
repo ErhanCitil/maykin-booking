0451278f385447f86eafb3c0af50d408f9acb2de
Erhan <erhancitil94@gmail.com>
2022-10-05 08:45:05 +0200

    Views for the city's

--- base/views.py
+++ base/views.py
@@ -17,3 +17,34 @@ def amsterdam(request):
         'data': Data.objects.filter(city_name='Amsterdam')
     }
     return render(request, 'amsterdam.html', context)
+
+def antwerpen(request):
+    context = {
+        'data': Data.objects.filter(city_name='Antwerpen')
+    }
+    return render(request, 'antwerpen.html', context)
+
+def athene(request):
+    context = {
+        'data': Data.objects.filter(city_name='Athene')
+    }
+    return render(request, 'athene.html', context)
+
+def bangkok(request):
+    context = {
+        'data': Data.objects.filter(city_name='Bangkok')
+    }
+    return render(request, 'bangkok.html', context)
+
+def barcelona(request):
+    context = {
+        'data': Data.objects.filter(city_name='Barcelona')
+    }
+    return render(request, 'barcelona.html', context)
+
+def berlijn(request):
+    context = {
+        'data': Data.objects.filter(city_name='Berlijn')
+    }
+    return render(request, 'berlijn.html', context)
+
