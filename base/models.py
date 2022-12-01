a12e4692e6bd00653cffd8e1d8f78f86667f862f
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-01 15:42:01 +0100

    git commit -am ¨Created model for Contact Form¨

--- base/models.py
+++ base/models.py
@@ -7,3 +7,17 @@ class Data(models.Model):
     city_name = models.CharField(max_length=100)
     hotel_id = models.CharField(max_length=100)
     hotel_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.city_name
+
+    
+class ContactForm(models.Model):
+    naamenachternaam = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    onderwerp = models.CharField(max_length=100)
+    bericht = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.naamenachternaam
+    
