b3930a753a54db96cdda4b87390eee07b2c03056
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-01 15:24:17 +0100

    v1

--- maykintest/settings.py
+++ maykintest/settings.py
@@ -31,10 +31,6 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-# Dit gebruik ik om bestanden zoals css, js en foto's te kunnen gebruiken in de templates.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new
 
 INSTALLED_APPS = [
     'jazzmin',
@@ -125,8 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+# Dit gebruik ik om bestanden zoals css, js en foto's te kunnen gebruiken in de templates.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
