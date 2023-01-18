824df579ab016a5933c490751524844050c436e3
Erhan <erhan.citil@maykinmedia.nl>
2023-01-18 12:31:08 +0100

    [#497] Replaced the manually csv writer to the csv library

--- base/management/commands/email_csv.py
+++ base/management/commands/email_csv.py
@@ -2,6 +2,9 @@ from django.core.mail import EmailMessage, send_mail
 from django.core.management.base import BaseCommand
 from base.models import City, Hotel
 
+import csv
+from django.http import HttpResponse
+
 class Command(BaseCommand):
 
     help = "Create a CSV or PDF file with the data from the database and sent it to the user"
@@ -12,28 +15,23 @@ class Command(BaseCommand):
 
         file = open('data.csv', 'w')
 
+        response = HttpResponse(content_type='text/csv', headers={'Content-Disposition': 'attachment; filename="data.csv"'})
+        writer = csv.writer(response)
+        
         for h in hotel:
-
-            file.write(f'{h.city.city_name};{h.hotel_name};{h.hotel_id}')
-
+            writer.writerow([h.city.city_name, h.city.city_id, h.hotel_name, h.hotel_id])
+        
         file.close()
 
-        mail = EmailMessage(
-
+        email = EmailMessage(
             'CSV file',
-
-            'This is the CSV file with the data from the database',
-
+            'CSV file',
             'test@gmail.nl',
-
             ['erhan.citil@maykinmedia.nl'],
-
         )
 
-        mail.attach_file('data.csv')
-
-        mail.send()
-
-        #  Attachment files kunnen niet verstuurd worden met de methode send_mail(), daarom heb ik gebruik gemaakt van EmailMessage. 
-
+        email.attach_file('data.csv')
+        email.send()
+    
+    #  Attachment files kunnen niet verstuurd worden met de methode send_mail(), daarom heb ik gebruik gemaakt van EmailMessage. 
     #  send_mail maakt de variabele een integer, returnt een 0 of een 1 en daarom krijg je een error als je een attachment file probeert te versturen.
