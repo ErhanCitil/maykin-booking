d9120ed51232f065966180e930e88942ebff68c5
Erhan <erhan.citil@maykinmedia.nl>
2023-01-18 15:15:01 +0100

    [#508] Added a basic pagination to the listview, will work on the UI

--- base/views.py
+++ base/views.py
@@ -14,14 +14,19 @@ class Index(generic.ListView):
 
 # Class-Based View voor de stad pagina ik geef de stad naam mee als parameter. Altijd met een hoofdletter de naam van een class
 class Stad(generic.ListView):
+    paginate_by = 4
     model = Hotel
     template_name = 'stad.html'
     context_object_name = 'data'
 
+    def get_queryset(self):
+        return Hotel.objects.filter(city__city_name=self.kwargs['city_name'])
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['data'] = Hotel.objects.filter(city__city_name=self.kwargs['city_name'])
+        context['city_name'] = self.kwargs['city_name']
         return context
+
 
 class HotelDetail(generic.ListView):
     model = City
