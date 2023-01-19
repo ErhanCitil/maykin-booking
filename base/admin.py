4deea2d9f4a3aaff2a690c44b23748e9158b65ea
Erhan <erhan.citil@maykinmedia.nl>
2023-01-19 12:09:24 +0100

    [#518] Added Room as an admin inline in the Hotel admin

--- base/admin.py
+++ base/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import City, Hotel
+from .models import *
 from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 # Hier mee registreer ik de Database model in de admin pagina.
 class CityAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     pass    
 
+class RoomAdmin(admin.TabularInline):
+    model = Room
+
 class HotelAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    pass
+    inlines = [RoomAdmin,]
 
 admin.site.register(Hotel, HotelAdmin)
 admin.site.register(City, CityAdmin)
