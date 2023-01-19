819062f48c8483ca90066aea0533ebd24ac53617
Erhan <erhan.citil@maykinmedia.nl>
2023-01-19 12:31:06 +0100

    [#518] Modified names of the model & added blank and null is true to some fields

--- base/models.py
+++ base/models.py
@@ -3,27 +3,29 @@ from django.db import models
 # Hier maak ik een database model aan met de naam Data. 
 class City(models.Model):
     city_id = models.CharField(max_length=100)
-    city_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.city_name
+        return self.name
 
 class Hotel(models.Model):
     city = models.ForeignKey(City, related_name='test', on_delete=models.CASCADE)
     hotel_id = models.CharField(max_length=100)
-    hotel_name = models.CharField(max_length=100)
-    hotel_image = models.ImageField(upload_to='hotel_img/', default='default.jpg')
-    description = models.TextField(default='')
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='hotel_img/', null=True, blank=True)
+    description = models.TextField(default='', blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.hotel_name
+        return self.name
 
 class Room(models.Model):
     hotel = models.ForeignKey(Hotel, related_name='room', on_delete=models.CASCADE)
-    room_image = models.ImageField(upload_to='room_img/')
-    room_title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='room_img/')
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    description = models.TextField(default='', blank=True, null=True)
 
     def __str__(self):
-        return self.room_title
+        return self.ritle
