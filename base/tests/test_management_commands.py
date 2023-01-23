01df6be199f0ac50737e88fc3e67b7c5d61f7bf3
Erhan <erhan.citil@maykinmedia.nl>
2023-01-23 08:10:07 +0100

    [#509] Added test for email csv management command

--- base/tests/test_management_commands.py
+++ base/tests/test_management_commands.py
@@ -2,7 +2,7 @@ from ..models import Hotel, Room, City
 from django.test import TestCase
 from .factories import CityFactory, HotelFactory, RoomFactory
 from django.core.management import call_command
-
+from django.core import mail 
 class DataFrameTest(TestCase):
     def test_dataframe_management_command(self):
         call_command('dataframe')
@@ -11,3 +11,8 @@ class DataFrameTest(TestCase):
     def test_fileimport_management_command(self):
         call_command('fileimport')
         self.assertEqual(City.objects.count(), 196)
+
+class EmailCSVTest(TestCase):
+    def test_emailcsv_management_command(self):
+        call_command('email_csv')
+        self.assertEqual(len(mail.outbox), 1)
