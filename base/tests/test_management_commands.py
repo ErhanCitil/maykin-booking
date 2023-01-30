1b013a335e563ab7ea196b81c4b04f82e3c3d37c
Erhan <erhan.citil@maykinmedia.nl>
2023-01-30 10:29:29 +0100

    [#509] Fixed failing tests

--- base/tests/test_management_commands.py
+++ base/tests/test_management_commands.py
@@ -1,16 +1,17 @@
 from ..models import Hotel, Room, City
 from django.test import TestCase
-from .factories import CityFactory, HotelFactory, RoomFactory
 from django.core.management import call_command
 from django.core import mail 
 class DataFrameTest(TestCase):
     def test_dataframe_management_command(self):
         call_command('dataframe')
-        self.assertEqual(City.objects.count(), 196)
+        self.assertEqual(City.objects.count(), 6)
+        self.assertEqual(Hotel.objects.count(), 196)
 
     def test_fileimport_management_command(self):
         call_command('fileimport')
-        self.assertEqual(City.objects.count(), 196)
+        self.assertEqual(City.objects.count(), 6)
+        self.assertEqual(Hotel.objects.count(), 196)
 
 class EmailCSVTest(TestCase):
     def test_emailcsv_management_command(self):
