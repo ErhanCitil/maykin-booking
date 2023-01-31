76d2c65e3f0672efad0248b57ed8af81362174b2
Erhan <erhan.citil@maykinmedia.nl>
2023-01-31 14:47:08 +0100

    [#534] Added test for the highlight

---
+++ base/tests/test_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from .factories import HotelFactory, RoomFactory, CityFactory, HighlightFactory
+from django.urls import reverse
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.city = CityFactory()
+        self.highlight = HighlightFactory(name='Free wifi')
+        self.hotel = HotelFactory(highlight=[self.highlight])
+        self.room = RoomFactory(hotel=self.hotel)
+
+    def test_hotel_detail(self):
+        response = self.client.get(reverse('hotel_detail', args=[self.hotel.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('hotel.html')
+    
+    def test_highlight_hotel(self):
+        response = self.client.get(reverse('hotel_detail', args=[self.highlight.id]))
+        self.assertContains(response, self.highlight.name)
