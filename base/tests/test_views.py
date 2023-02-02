4e4a523efe877230b2e86caa0dd5bac8e0af7264
Erhan <erhan.citil@maykinmedia.nl>
2023-02-02 11:07:34 +0100

    [#546] Added some tests but failing at the changing details test

--- base/tests/test_views.py
+++ base/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from .factories import HotelFactory, RoomFactory, CityFactory, HighlightFactory
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django_webtest import WebTest
 
 class TestViews(TestCase):
     def setUp(self):
@@ -22,3 +24,48 @@ class TestViews(TestCase):
         response = self.client.get(reverse('hotel_edit', args=[self.hotel.id]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('hotel_edit.html')
+
+class TestLogin(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='Alex', password='Landgraaf123')
+        self.city = CityFactory()
+        self.highlight = HighlightFactory(name='Free renting a bike')
+        self.hotel = HotelFactory(highlight=[self.highlight])
+        self.room = RoomFactory(hotel=self.hotel)
+
+    def test_login(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('login.html')
+
+    def test_hotel_edit_page_redirects_to_login(self):
+        # Hier test ik dat de gebruiker word doorgestuurd als diegene niet is ingelogd dus de gebruiker kan de edit pagina niet bereiken als hij/zij niet is ingelogd.
+        response = self.client.get(reverse('hotel_edit', args=[self.hotel.id]))
+        self.assertRedirects(response, '/login/?next=/hotel_edit/{}'.format(self.hotel.id))
+
+    def test_user_logged_in(self):
+        self.client.login(username='Alex', password='Landgraaf123')
+        response = self.client.get(reverse('hotel_edit', args=[self.hotel.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('hotel_edit.html')
+
+class EditPage(WebTest):
+    def setUp(self):
+        self.user = User.objects.create_user(username='Alex', password='Landgraaf123')
+        self.city = CityFactory()
+        self.highlight = HighlightFactory(name='Free renting a bike')
+        self.hotel = HotelFactory(highlight=[self.highlight], price=100.00)
+        self.room = RoomFactory(hotel=self.hotel)
+
+    def test_user_change_details(self):
+        self.client.login(username='Alex', password='Landgraaf123')
+        response = self.client.get(reverse('hotel_edit', args=[self.hotel.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('hotel_edit.html')
+        self.assertIn('form', response.context)
+        form = response.context['form']
+        self.assertEqual(form.instance, self.hotel)
+        self.assertEqual(form.instance.price, 100.00)
+        form.instance.price = 200.00
+        form.submit().follow()
+        self.assertEqual(self.hotel.price, 200.00)
