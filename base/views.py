345fc871721cb8406fcdb061b8d1246084c96b67
Erhan <erhan.citil@maykinmedia.nl>
2023-02-03 10:18:01 +0100

    [#533] Context data in OrderPDF order token

--- base/views.py
+++ base/views.py
@@ -138,7 +138,6 @@ class OrderPDF(WeasyTemplateResponseMixin, generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['order'] = Order.objects.get(id=self.kwargs['pk'])
         self.request.session['order_token'] = str(context['order'].token)
         return context
 
