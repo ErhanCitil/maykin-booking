43bbd5898106fdcfcf51e867a51549514f8e512c
Erhan <erhan.citil@maykinmedia.nl>
2023-02-14 11:31:16 +0100

    [#35] Added upload to Hotel model field and created model field

--- base/models.py
+++ base/models.py
@@ -21,6 +21,7 @@ class Hotel(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     is_available = models.BooleanField(default=True)
     terms = RichTextField(blank=True, null=True)
+    upload = models.ImageField(upload_to='hotel_img/', null=True, blank=True)
     def __str__(self):
         return self.name
 
